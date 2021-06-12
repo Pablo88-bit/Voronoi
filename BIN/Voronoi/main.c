e29b9291f162c9bc6675e9d2bf0bed8ba8d44b02
Antonio Escorcia <packageinstaller2@gmail.com>
2021-06-12 15:43:58 -0600

    Limpieza de archivo main
    Se limpio el archivo main, se borro todo dentro de la funcion main

--- BIN/Voronoi/main.c
+++ BIN/Voronoi/main.c
@@ -3,19 +3,6 @@
 
 int main()
 {
-	printf("Juan");
-
-
-
-	printf("Brayan Josue Potosme Rivera");
-	printf("soy un mae tranquilo, tengo 18 años y me gusta jugar video juegos");
-	printf("Marvin Sevilla ");
-	printf("Hola Soy marvin, me gusta el anime y cocinar uwu");
-    	printf("Cris Lopez");
-    printf("Soy fan del arte");
-
-
-	printf("Pablo Mairena");
-	printf("Me gustan mucho los deportes y el ejercicio fisico");
+	
 	return 0;
 }
